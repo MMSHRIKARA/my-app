a637c4880e4303943b1284bf00c41f27ec90d5e5
MMSHRIKARA <shrikara21@gmail.com>
2022-09-06 19:58:51 +0530

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentOverviewComponent } from './component-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cov', component: ComponentOverviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
